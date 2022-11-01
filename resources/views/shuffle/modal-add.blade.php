84bed9f07cdafda6d0b16551431ab559b50eb934
satria girinanda <satriagn93@gmail.com>
2022-11-01 08:31:56 +0700

    v1

--- resources/views/shuffle/modal-add.blade.php
+++ resources/views/shuffle/modal-add.blade.php
@@ -1,82 +1,25 @@
-<div class="modal fade" id="edit-Items" tabindex="-1" aria-hidden="true">
-    <!--begin::Modal dialog-->
-    <div class="modal-dialog modal-dialog-centered mw-600px">
-        <!--begin::Modal content-->
-        <div class="modal-content">
-            <!--begin::Modal header-->
-            <div class="modal-header">
-                <!--begin::Modal title-->
-                <h2> </h2>
+<div style="background-color: rgb(18 18 27 / 52%);" class="modal fade" id="edit-Items" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered mw-650px">
+        <div style="background-color: #12121b; border-color: white; border: 5px;" class="modal-content">
+            <div class="modal-header" style="text-align: center;">
+                <h3 class="modal-title"><img style="text-align: center;vertical-align: middle;" src="{{asset('template')}}/media/doorprize/congrat2.png" alt="Paris" width="90%" ;></h3>
             </div>
-            <!--end::Modal header-->
-            <!--begin::Modal body-->
-            <div class="modal-body py-lg-10 px-lg-10">
-                <div class="mh-475px scroll-y me-n7 pe-7">
-                    <div class="text-center mb-13">
-                        <h1 style="font-size: 30px;" class="d-flex justify-content-center align-items-center mb-3">
-
-                            <img src="{{asset('template')}}/media/icons/gift.svg" class="me-4 w-65px" style="border-radius: 4px" alt="" />
-                            Selamat anda mendapatkan doorprize &nbsp
-                            <img src="{{asset('template')}}/media/icons/gift.svg" class="me-4 w-65px" style="border-radius: 4px" alt="" />
-                        </h1>
-                    </div>
-                    <form id="form-create" method="POST" action="{{ route('doorprize.store') }}" enctype="multipart/form-data">
-                        @csrf
-                        <div class="border border-hover-primary p-7 rounded mb-7">
-                            <!--begin::Info-->
-                            <div class="d-flex flex-stack pb-3">
-                                <!--begin::Info-->
-                                <div class="text-center d-flex">
-                                    <!-- <img class="mw-100 h-200px me-4 mb-5 mb-lg-0" src="{{asset('template')}}/media/illustrations/sigma-1/11.png"> -->
-                                    <!--end::Avatar-->
-                                    <!--begin::Details-->
-                                    <div style="margin-top: 50px;" class="ms-5">
-                                        <!--begin::Name-->
-                                        <div style="text-align: center;" class="d-flex align-items-center">
-                                            <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="name_model" name="name_model" readonly>
-                                        </div>
-                                        <br>
-                                        <!--begin::Name-->
-                                        <div class="d-flex align-items-center">
-                                            <input style="font-size: 20px;" type="text" class="form-control form-control-sm" id="number_model" name="number_model" readonly>
-                                            <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="doorprize_model" name="doorprize_model" hidden>
-                                            <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="email_model" name="email_model" hidden>
-                                            <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="company_model" name="company_model" hidden>
-                                        </div>
-                                    </div>
-                                    <!--end::Details-->
-                                </div>
-                            </div>
-                            <!--end::Info-->
-                            <!--begin::Wrapper-->
-                            <div class="p-0">
-                                <div class="d-flex flex-column">
-                                    <!--begin::Separator-->
-                                    <div class="separator separator-dashed border-muted my-5"></div>
-                                    <!--end::Separator-->
-                                    <!--begin::Action-->
-                                    <div class="d-flex flex-stack">
-                                        <!--begin::Progress-->
-                                        <div class="d-flex flex-column mw-200px">
-                                            <div class="d-flex align-items-center mb-2">
-                                                <a onclick="cancelparticipant()" class="btn btn-sm btn-danger">Cancel</a>
-                                            </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-sm btn-primary">Continue</button>
-                                        <!--end::Button-->
-                                    </div>
-                                    <!--end::Action-->
-                                </div>
-                                <!--end::Footer-->
-                            </div>
-                            <!--end::Wrapper-->
-                        </div>
-                    </form>
+            <form id="form-create" method="POST" action="{{ route('doorprize.store') }}" enctype="multipart/form-data">
+                @csrf
+                <div style="color: wheat; text-align: center;" class="modal-body">
+                    <p style="font-size: 20px;">Anda telah terpilih mendapatkan doorprize</p>
+                    <input style="font-size: 35px; border: #12121b; text-align: center;" type="text" class="form-control form-control-sm" id="number_model" name="number_model" readonly>
+                    <input style="font-size: 30px; width: 20px;" type="text" class="form-control form-control-sm" id="doorprize_model" name="doorprize_model" hidden>
+                    <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="email_model" name="email_model" hidden>
+                    <input style="font-size: 30px;" type="text" class="form-control form-control-sm" id="company_model" name="company_model" hidden>
+                    <input style="font-size: 45px; border: #12121b; text-align: center;" type="text" class="form-control form-control-sm" id="name_model" name="name_model" readonly>
                 </div>
-            </div>
-            <!--end::Modal body-->
+
+                <div class="modal-footer">
+                    <a onclick="cancelparticipant()" class="btn btn-sm btn-danger">Cancel</a>
+                    <button type="submit" class="btn btn-sm btn-primary">Continue</button>
+                </div>
+            </form>
         </div>
-        <!--end::Modal content-->
     </div>
-    <!--end::Modal dialog-->
 </div>
---
+++ resources/views/shuffle/modal-tes.blade.php
@@ -0,0 +1,24 @@
+<div class="modal fade" tabindex="-1" id="kt_modal_1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title">Modal title</h3>
+
+                <!--begin::Close-->
+                <div class="btn btn-icon btn-sm btn-active-light-primary ms-2" data-bs-dismiss="modal" aria-label="Close">
+                    <span class="svg-icon svg-icon-1"></span>
+                </div>
+                <!--end::Close-->
+            </div>
+
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
